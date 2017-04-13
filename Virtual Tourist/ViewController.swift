9f08662cfbfcd7d8303430d29ebe17348103b208
brahma <brahma@gopassport.com>
2017-04-13 13:25:46 +0530

    initial commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Brahma Reddy Chilakala on 13/04/17.
+//  Copyright © 2017 Brahma Reddy Chilakala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
