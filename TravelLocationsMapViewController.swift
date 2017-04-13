ebe1639bbf84cb604bbb5a6595b11c2c4d87e242
brahma <brahma@gopassport.com>
2017-04-13 18:15:05 +0530

    add gesture recognizer

--- TravelLocationsMapViewController.swift
+++ TravelLocationsMapViewController.swift
@@ -8,21 +8,56 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class TravelLocationsMapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var manager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(TravelLocationsMapViewController.longpress(gestureRecognizer:)))
+        uilpgr.minimumPressDuration = 2
+        mapView.addGestureRecognizer(uilpgr)
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            let touchPoint = gestureRecognizer.location(in: self.mapView)
+            let newCoordinate = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            var title = ""
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    if let placemark = placemarks?[0] {
+                        if placemark.subThoroughfare != nil {
+                            title += placemark.subThoroughfare! + " "
+                        }
+                        if placemark.thoroughfare != nil {
+                            title += placemark.thoroughfare!
+                        }
+                    }
+                }
+                
+                if title == "" {
+                    title = "Added \(NSDate())"
+                }
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                self.mapView.addAnnotation(annotation)
+            })
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
 
     /*
