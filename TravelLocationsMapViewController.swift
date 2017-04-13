9681284c11fba37b59c20aa301755e7f6ed44a85
brahma <brahma@gopassport.com>
2017-04-13 13:48:11 +0530

    add map view

---
+++ TravelLocationsMapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TravelLocationsMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Brahma Reddy Chilakala on 13/04/17.
+//  Copyright © 2017 Brahma Reddy Chilakala. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Virtual Tourist/TravelLocationsMapViewController.swift
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
